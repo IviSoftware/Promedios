0ff5c70ffdb87a5b74eff6da979cd3115bc4f281
IviSoftware <iva_men_ase@hotmail.com>
2022-09-15 19:24:48 -0500

    terminado

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const btnAdd = document.querySelector('.btn_add');
 const containerMat = document.querySelector('.materias');
 const containerCal = document.querySelector('.calificaciones');
 const btnDelete = document.querySelector('.btn-delete');
+const getPromed = document.querySelector('.btn_promedio');
+const promedioResul = document.querySelector('.result');
 
 function addCourse(){
   const matInput = document.createElement('input');
@@ -10,7 +12,7 @@ function addCourse(){
   matInput.placeholder='Materia';
 
   const calInput = document.createElement('input');
-  calInput.className='txt_user txt_course';
+  calInput.className='txt_user txt_cal';
   calInput.type='text';
   calInput.placeholder='Calificacion';
 
@@ -34,6 +36,24 @@ function deleteCourse(){
 
 }
 
+function getPromedio(){
+  const nodesMaterias = document.querySelectorAll('.txt_course');
+  const nodesCalificaciones = document.querySelectorAll('.txt_cal');
+  
+  const listMaterias = Array.apply(null,nodesMaterias).map(item=> item.value);
+  const listCalificaciones = Array.apply(null,nodesCalificaciones).map(item=> parseInt(item.value) );
+  
+  const sumCal = listCalificaciones.reduce((acc,item)=> acc + item);
+  const result = sumCal / listMaterias.length;
+  
+  if(result){
+    promedioResul.innerText = `Tu promedio es : ${result}`;
+  }else{
+    promedioResul.innerText = 'Ingresa calificaciones';
+  }
+
+}
+
 btnAdd.addEventListener('click',addCourse);
 btnDelete.addEventListener('click',deleteCourse);
-
+getPromed.addEventListener('click',getPromedio);
